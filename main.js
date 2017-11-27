a6a766481e6ec734187ede82139a6c851e36f324
innej99 <jmunoz91@phm.k12.in.us>
2017-11-27 15:25:07 -0500

    files for website
    might not work bc of materialize lmao

---
+++ main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function(){
+  $(".button-collapse").sideNav();
+  $('select').material_select();
+  $('.slider').slider();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+})
