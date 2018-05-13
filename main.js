cab1067c83f4fb253bc6b7a2b28658ae03153bf2
Jenni <mmunozjennifer@gmail.com>
2018-05-13 04:41:25 -0400

    Add files via upload
    changed link colors to black, added hover features to links and images

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-$( document ).ready(function(){
-  $(".button-collapse").sideNav();
-  $('select').material_select();
-  $('.slider').slider();
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-})
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const triangle = [ [0, -20], [20, 20], [-20, 20] ];
+canvas.addEventListener("mousedown", mouseDown);
+canvas.addEventListener("contextmenu", e => e.preventDefault() );
+function mouseDown(event) {
+  const bounds = event.target.getBoundingClientRect();
+  const x = event.pageX - bounds.left - scrollX;
+  const y = event.pageY - bounds.top  - scrollY;
+  if (event.which === 3) { drawShape(x, y, triangle) }
+}
+function drawShape(x, y, shape) {
+  var i = 0;
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  while (i < shape.length) { ctx.lineTo( shape[i][0] + x, shape[i++][1] + y ) }
+  ctx.fill();
+}
